c4cea64563ad970343f395db9c6ccb1eae5851aa
trex1004 <trex1004@gmail.com>
2026-06-11 23:18:29 +0900

    주석추가 및 개터,세터 메서드 수정 기록 삭제 메서드 추가, 연산자 예외처리

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Calculator {
     public int calculate(int a, int b, char operator) {
@@ -10,12 +11,8 @@ public class Calculator {
             case '+' -> result = a + b;
             case '-' -> result = a - b;
             case '*' -> result = a * b;
-            case '/' -> {
-                if (b == 0) {
-                    throw new ArithmeticException();
-                }
-                result = a / b;
-            }
+            case '/' -> result = a / b;
+            default -> throw new RuntimeException("연산자가 아닙니다.");
 
         }
         record.add(result);
@@ -23,13 +20,18 @@ public class Calculator {
     }
 
 
-    private static ArrayList<Integer> record = new ArrayList<>();
+    private List<Integer> record = new ArrayList<>(); //유연한 기록공유를 위해static제거와 List인터페이스로 로변경
 
-    public void getRecord() {
-        System.out.println("목록\n" + record);
+
+    public List<Integer> getRecord() { // 게터 메서드에서 출력까지 X
+        return this.record;
     }
 
-    public void setRecord() {
+    public void setRecord(List<Integer> record) {
+        this.record = record;
+    }
+
+    public void removeRecord() { //세터메서드에서 삭제기능 제거
         record.remove(0);
     }
 }
